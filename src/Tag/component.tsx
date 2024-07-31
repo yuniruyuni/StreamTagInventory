807650b7c4a10ae6285ab24559a0f25a984cb4ea
yuniruyuni <yuniruyuni@gmail.com>
2024-08-01 03:22:12 +0900

    feat: re-design tag component

--- src/Tag/component.tsx
+++ src/Tag/component.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import type React from 'react';
 
 type Props = {
@@ -6,16 +7,38 @@ type Props = {
 };
 
 export const Tag: React.FC<Props> = ({ onClose, children }) => (
-  <span className='font-medium rounded inline-flex items-center bg-green-500 text-white'>
-    <span className='py-1 px-1 text-sm mr-1 mb-1'>{children}</span>
+  <span
+    id="badge-dismiss-default"
+    className={clsx(
+      "inline-flex items-center",
+      "rounded",
+      "px-2 py-1 me-2",
+      "text-sm font-medium",
+      "text-blue-800 bg-blue-100",
+      "dark:bg-blue-900 dark:text-blue-300",
+    )}
+  >
+    {children}
     {onClose && (
       <button
         type="button"
-        className="inline-flex items-center border-l h-full w-hull cursor-pointer py-1 px-1 text-sm"
+        className={clsx(
+          "inline-flex items-center",
+          "p-1 ms-2",
+          "text-sm text-blue-400 bg-transparent",
+          "rounded-sm",
+          "hover:bg-blue-200 hover:text-blue-900 dark:hover:bg-blue-800 dark:hover:text-blue-300",
+        )}
+        data-dismiss-target="#badge-dismiss-default"
+        aria-label="Remove"
         onClick={onClose}
       >
-        x
+        <svg className="w-2 h-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+        </svg>
+        <span className="sr-only">Remove tag</span>
       </button>
     )}
   </span>
 );
+
