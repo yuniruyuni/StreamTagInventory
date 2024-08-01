dd3daf062f3c9fc28552816475e834db93404fb8
yuniruyuni <yuniruyuni@gmail.com>
2024-08-01 17:19:13 +0900

    feat: change template edit interface with direct edit support

--- src/model/template.ts
+++ src/model/template.ts
@@ -1,4 +1,5 @@
-import type { Category } from '~/model/category';
+import { ulid } from 'ulid';
+import { type Category, newCategory } from '~/model/category';
 
 export type Template = {
   id: string;
@@ -6,3 +7,28 @@ export type Template = {
   category: Category;
   tags: string[];
 };
+
+
+export function newTemplate(): Template {
+  return {
+    id: ulid(),
+    title: "",
+    category: newCategory(),
+    tags: [],
+  };
+}
+
+export function cloneTemplate(template: Template): Template {
+  return {...template, id: ulid() };
+}
+
+
+export function validateTemplate(template: Template): boolean {
+  if (template.title === "") return false;
+  if (template.category.id === "") return false;
+  if (template.tags.length === 0) return false;
+  if (10 < template.tags.length) return false;
+  if (template.tags.some((tag) => tag === "")) return false;
+
+  return true;
+}
