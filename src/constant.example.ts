931c0b4af8d30b63524a810b5fc5b0ef4effd79a
rdonster@gmail.com <rdonster@gmail.com>
2024-10-01 14:04:11 +0900

    Remove constant.ts and add content.ts example template.

---
+++ src/constant.example.ts
@@ -0,0 +1,7 @@
+/**
+ * This file serves as a configuration template. Please follow these steps:
+ * 1. Copy this file and rename it to `constant.ts`.
+ * 2. Update the values below according to your environment.
+ */
+
+export const CLIENT_ID = "your_client_id_here";
--- src/constant.ts
+++
@@ -1,1 +0,0 @@
-export const CLIENT_ID = "d2kz8x5se7k6b1n0picux0r7kaozi3";
